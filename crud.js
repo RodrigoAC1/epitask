36c8c3e83b93cfcf8b7279342d791fd3c874ec5c
RodrigoAC1 <rocavalheiro1@gmail.com>
2023-05-16 22:47:27 -0300

    main

--- crud.js
+++ crud.js
@@ -3,10 +3,25 @@ document.querySelector("#salvar").addEventListener("click", cadastrar)
 let lista_produtos = []
 
 window.addEventListener("load", () => {
-    lista_produtos = JSON.parse (localStorage.getItem("lista_produtos"))
-    lista_produtos.forEach((produtos) => {
-        document.querySelector("#produtos").innerHTML += gerarCard(produtos)
-    })
+    lista_produtos = JSON.parse (localStorage.getItem("lista_produtos")) || []
+    atualizar()
+})
+
+document.querySelector("#indisponivel").addEventListener("click", () => {
+    lista_produtos.filter(produtos => !produtos.disponivel)
+    atualizar()
+})
+
+document.querySelector("#busca").addEventListener("keyup", () => {
+    const nome =  document.querySelector("#busca").value
+    lista_produtos = lista_produtos.filter(produtos => produtos.nome.includes(nome))
+    atualizar()
+})
+
+document.querySelector("#disponivel").addEventListener("click", () => {
+    lista_produtos = JSON.parse (localStorage.getItem("lista_produtos")) || []
+    lista_produtos.filter(produtos => produtos.disponivel)
+    atualizar()
 })
 
 function cadastrar(){
@@ -17,10 +32,12 @@ function cadastrar(){
     let valor = document.querySelector("#valor").value
 
     const produtos = {
+        id: Date.now(),
         nome,
         descricao,
         tipo,
         valor,
+        vendido: false
     }
 
 
@@ -36,17 +53,42 @@ function cadastrar(){
     document.querySelector("#nome").value=''
     document.querySelector("#descricao").value=''
 
-    localStorage.setItem("lista_produtos", JSON.stringify(lista_produtos))
-
+    salvar()
     modal.hide()
 
 }
 
-function apagar(botao){
-    botao.parentNode.parentNode.parentNode.remove()
+function atualizar(){
+    document.querySelector("#produtos").innerHTML = ""
+    lista_produtos.forEach((produtos) => {
+        document.querySelector("#produtos").innerHTML += gerarCard(produtos)
+    })
+}
+
+function salvar(){
+    localStorage.setItem("lista_produtos", JSON.stringify(lista_produtos))
+}
+
+function apagar(id){
+    lista_produtos = lista_produtos.filter((produtos) => {
+      return produtos.id != id  
+    }) //arrow function
+    salvar()
+    atualizar()
+}
+
+function disponivel(id){
+    let produtos_encontrados = lista_produtos.find((produtos) => {
+        return produtos.id == id
+    })
+    produtos_encontrados.disponivel = true
+    salvar()
+    atualizar()
+
 }
 
 function gerarCard(produtos){
+    const indisponivel = (produtos.disponivel) ? "disabled" : ""
     return `<div class="col-12 col-md-6 col-lg-3">
                 <div class="card">
                     <div class="card-header">
@@ -60,10 +102,10 @@ function gerarCard(produtos){
                             </span>
                         </p>
                         <p>${produtos.valor}pts</p>
-                        <a href="#" class="btn btn-success">
+                        <a href="#" onClick='disponivel(${produtos.id}) 'class="btn btn-success ${indisponivel}">
                             <i class="bi bi-check-lg"></i>
                         </a>
-                        <a href="#" onClick='apagar(this) 'class="btn btn-danger">
+                        <a href="#" onClick='apagar(${produtos.id}) 'class="btn btn-danger">
                             <i class="bi bi-trash"></i>
                         </a>
                     </div>
